348cb929ae14c4c2b6b07325f97ead6b8cfb4245
Kim Gwang Je <gwangjeg14@gmail.com>
2024-07-10 17:25:10 +0900

    20240710

---
+++ src/Algorithm/BaekJoon/Ex10816.java
@@ -0,0 +1,58 @@
+package Algorithm.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 문제
+ * 숫자 카드는 정수 하나가 적혀져 있는 카드이다. 상근이는 숫자 카드 N개를 가지고 있다. 정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드를 상근이가 몇 개 가지고 있는지 구하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 첫째 줄에 상근이가 가지고 있는 숫자 카드의 개수 N(1 ≤ N ≤ 500,000)이 주어진다. 둘째 줄에는 숫자 카드에 적혀있는 정수가 주어진다. 숫자 카드에 적혀있는 수는 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다.
+ *
+ * 셋째 줄에는 M(1 ≤ M ≤ 500,000)이 주어진다. 넷째 줄에는 상근이가 몇 개 가지고 있는 숫자 카드인지 구해야 할 M개의 정수가 주어지며, 이 수는 공백으로 구분되어져 있다. 이 수도 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다.
+ *
+ * 출력
+ * 첫째 줄에 입력으로 주어진 M개의 수에 대해서, 각 수가 적힌 숫자 카드를 상근이가 몇 개 가지고 있는지를 공백으로 구분해 출력한다.
+ *
+ * 예제 입력 1
+ * 10
+ * 6 3 2 10 10 10 -10 -10 7 3
+ * 8
+ * 10 9 -5 2 3 4 5 -10
+ * 예제 출력 1
+ * 3 0 0 1 2 0 0 2
+*/
+
+public class Ex10816 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            if(arr[i] < min) min = arr[i];
+            if(arr[i] > max) max = arr[i];
+        }
+        int[] num = new int[max - min + 1];
+        for(int i = 0; i < N; i++){
+            num[arr[i]-min]++;
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        int[] answer = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M; i++){
+            int a = Integer.parseInt(st.nextToken());
+            if (a < min || a > max) {
+                System.out.println(0);
+            } else {
+                System.out.println(num[a - min]);
+            }
+        }
+    }
+}
