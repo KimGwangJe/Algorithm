67284d40b099829bba6067a4b7529466ca576de8
Kim Gwang Je <gwangjeg14@gmail.com>
2024-12-13 20:30:41 +0900

    Insertion Sort, Selection Sort 다시

--- src/Sort/InsertionSort.java
+++ src/Sort/InsertionSort.java
@@ -1,67 +1,24 @@
 package Sort;
 
 public class InsertionSort {
+    public static int[] arr = {3,1,4,3,2};
     public static void main(String[] args){
-        int[] arr = {3,1,4,3,2};
-        int[] sum = new int[arr.length];
-        int result = 0;
-        for(int i = 1; i <arr.length; i++){
-            int min = arr[i];
-            int idx = i; // 들어갈 인덱스
-            for(int j = i-1; j >= 0; j--){
-                if(arr[i] > arr[j]){
-                    idx = j + 1;
+        int i = 0;
+        int j = 0;
+        for(i = 1; i < arr.length; i++){
+            int target = arr[i];
+            for(j = i - 1; j >= 0; j--){
+                if(arr[j]>target){
+                    arr[j+1] = arr[j];
+                } else{
                     break;
                 }
-                if(j == 0){
-                    idx = 0;
-                }
             }
-            for(int k = i; k > idx ; k--){
-                arr[k] = arr[k-1];
-            }
-            arr[idx] = min;
+            arr[j+1] = target;
         }
-        for(int a : arr){
-            System.out.println(a);
-        }
-        sum[0] = arr[0];
-        result += sum[0];
-        for(int i = 1; i < arr.length; i++){
-            sum[i] = sum[i-1] + arr[i];
-            result += sum[i];
-        }
-        System.out.println(result);
-        insert();
-    }
 
-    public static void insert(){
-        int[] arr = {3,1,4,3,2};
-        int[] sum = new int[arr.length];
-
-        for(int i = 0; i < arr.length - 1; i++){
-            int min = arr[i];
-            int minIndex = i;
-            for(int j = i + 1; j < arr.length; j++){
-                if(min > arr[j]){
-                    min = arr[j];
-                    minIndex = j;
-                }
-            }
-            for(int k = minIndex; k > i; k--){
-                arr[k] = arr[k-1];
-            }
-            arr[i] = min;
+        for(int num : arr){
+            System.out.println(num);
         }
-        for(int i: arr){
-            System.out.println(i);
-        }
-        sum[0] = arr[0];
-        int result = sum[0];
-        for(int i = 1; i < arr.length; i++){
-            sum[i] = sum[i-1] + arr[i];
-            result+=sum[i];
-        }
-        System.out.println(result);
     }
 }
--- src/Sort/SelectSort.java
+++ src/Sort/SelectSort.java
@@ -2,21 +2,17 @@ package Sort;
 
 public class SelectSort {
     static int[] arr = new int[]{42,32,24,60,15};
-    static int idx = 0;
 
     public static void main(String[] args){
-        for(int i = 0; i < arr.length - 1; i++){
-            int max = arr[i];
-            for(int j = i; j < arr.length; j++){
-                if(max < arr[j]){
-                    max = arr[j];
-                    idx = j;
+        for(int i = 0; i < arr.length; i++){
+            int min = i;
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[min] > arr[j]){
+                    min = j;
                 }
             }
-            swap(i,idx);
-        }
-        for(int i : arr){
-            System.out.print(i+" ");
+
+            swap(min, arr[i]);
         }
     }
 
