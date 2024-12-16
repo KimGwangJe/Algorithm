7ce0088b48d71cc3e1596d41f278520bd6575e5d
Kim Gwang Je <gwangjeg14@gmail.com>
2024-12-16 15:37:48 +0900

    20241216 정렬

--- src/Sort/MergeSort.java
+++ src/Sort/MergeSort.java
@@ -4,7 +4,7 @@ public class MergeSort {
     static int[] arr = {42,32,24,60,15,5,90,45};
     static int[] temp = new int[arr.length];
     public static void main(String[] args) {
-        mergeSort(0, arr.length-1);
+        mergeSort(0, arr.length - 1);
         for(int i : arr){
             System.out.println(i);
         }
@@ -12,32 +12,30 @@ public class MergeSort {
 
     public static void mergeSort(int left, int right){
         if(left == right) return;
+
         int mid = (left + right)/2;
-
         mergeSort(left, mid);
-        mergeSort(mid + 1, right);
-
-        for(int i = left; i <= right; i++){
-            temp[i] = arr[i];
-        }
+        mergeSort(mid+1, right);
 
         int l = left;
         int r = mid + 1;
         int idx = left;
 
         while(l <= mid && r <= right){
-            if(temp[l] <= temp[r]){
-                arr[idx++] = temp[l++];
-            } else{
-                arr[idx++] = temp[r++];
+            if(arr[l] <= arr[r]){
+                temp[idx++] = arr[l++];
+            } else {
+                temp[idx++] = arr[r++];
             }
         }
-
-        while (l <= mid) { // 왼쪽 남은 부분 복사
-            arr[idx++] = temp[l++];
+        while(l <= mid){
+            temp[idx++] = arr[l++];
         }
-        while (r <= right) { // 오른쪽 남은 부분 복사
-            arr[idx++] = temp[r++];
+        while(r <= right){
+            temp[idx++] = arr[r++];
+        }
+        for(int i = left; i <= right; i++){
+            arr[i] = temp[i];
         }
     }
 }
--- src/Sort/QuickSort.java
+++ src/Sort/QuickSort.java
@@ -1,52 +1,46 @@
 package Sort;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.Arrays;
 
 public class QuickSort {
+    public static int[] arr = {6,8,1,7,4,3,5,2};
     public static void main(String[] args){
-        int[] arr = {4,1,2,3,5};
-        Arrays.sort(arr);
-        quickSort(arr,0,arr.length-1);
+        quickSort(0, arr.length-1);
         for(int i : arr){
             System.out.println(i);
         }
     }
 
-    public static void quickSort(int[] arr){
-        quickSort(arr,0,arr.length - 1);
+    public static void quickSort(int left, int right){
+        if(left >= right) return;
+
+        int pivot = partition(left, right);
+        quickSort(left, pivot-1);
+        quickSort(pivot+1, right);
     }
 
-    public static void quickSort(int[] arr, int start, int end){
+    public static int partition(int left, int right){
+        int pivot = left;
+        int l = left+1;
+        int r = right;
 
-        if(start>= end) return;
+        while(l <= r){
+            while(l <= right && arr[l] <= arr[pivot]) l++;
+            while(r > left && arr[r] >= arr[pivot]) r--;
 
-        int pivot = start;
-        int low = start + 1;
-        int high = end;
-
-        while(low<=high){
-            while(low<=end && arr[low] <= arr[pivot]) low++;
-            while(high>start && arr[high] >= arr[pivot]) high--;
-
-            // low가 더 크면 더이상 바꿀게 없기 때문에 pivot을 이동시킴
-            if(low>high){
-                swap(arr,high,pivot);
-            }else {
-                // 큰거 작은거 교체
-                swap(arr,low,high);
+            if(l < r){
+                swap(l, r);
             }
         }
-        // pivot은 그대로 두고 계속 진행함 자연스럽게 왼쪽 분할 정렬 끝
-        // pivot이 위치를 변경해도 새로운 피봇으로 high가 start와 같을때까지 반복
-        // start는 0 고정임
-        quickSort(arr,start,high-1);
-        // 여기에서는 pivot 위치 빠꿔서 오른쪽 분할 정렬
-        quickSort(arr,high+1,end);
+        swap(pivot, r);
+        return r;
     }
 
-    public static void swap(int[] arr, int i, int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    public static void swap(int left, int right){
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
     }
 }
